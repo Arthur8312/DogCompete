8cf849d708afecc38d586bef8bc86b54ac062287
ArthurChien <arthur12394@gmail.com>
2021-05-21 09:58:39 +0800

    97 weight

--- train_classifier.py
+++ train_classifier.py
@@ -18,7 +18,7 @@ from classification_models.keras import Classifiers
 feature_dim_2 = 499
 feature_dim_1 = 120
 channel = 1
-epochs = 10
+epochs = 20
 batch_size = 80
 verbose = 1
 num_classes = 6
@@ -50,12 +50,12 @@ y_test_hot = to_categorical(y_test)
 
 def audio_model():
   model = Sequential()
-  model.add(Conv2D(64, kernel_size=(3, 35), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel), padding='same'))
+  model.add(Conv2D(64, kernel_size=(3, 10), activation='relu', input_shape=(feature_dim_1, feature_dim_2, channel), padding='same'))
   model.add(MaxPooling2D(pool_size=(3, 1)))
   model.add(Conv2D(128, kernel_size=(7, 1), activation='relu', padding='same'))
   model.add(MaxPooling2D(pool_size=(4, 1)))
   model.add(Conv2D(256, kernel_size=(10, 1), activation='relu', padding='valid'))
-  model.add(Conv2D(512, kernel_size=(1, 35), activation='relu', padding='same'))
+  model.add(Conv2D(512, kernel_size=(1, 10), activation='relu', padding='same'))
   model.add(keras.layers.GlobalMaxPooling2D())
   model.add(Dropout(0.25))
   model.add(Dense(256, activation='relu'))
@@ -72,16 +72,16 @@ if not os.path.exists(weight_dir):
     os.mkdir(weight_dir)
 checkpoint = keras.callbacks.ModelCheckpoint(filepath=weight_dir+'/checkpoint-{epoch:02d}.hdf5')
 
-# model = audio_model()
+model = audio_model()
 # model = keras.models.load_model('model_log/checkpoint-150.hdf5')
-ResNet18, predata = Classifiers.get('resnet18')
-base_model = ResNet18((feature_dim_1, feature_dim_2, channel), include_top=False)
-x = keras.layers.GlobalAveragePooling2D()(base_model.output)
-output = keras.layers.Dense(6, activation='softmax')(x)
-model = keras.models.Model(inputs=[base_model.input], outputs=[output])
+# ResNet18, predata = Classifiers.get('resnet18')
+# base_model = ResNet18((feature_dim_1, feature_dim_2, channel), include_top=False)
+# x = keras.layers.GlobalAveragePooling2D()(base_model.output)
+# output = keras.layers.Dense(6, activation='softmax')(x)
+# model = keras.models.Model(inputs=[base_model.input], outputs=[output])
 
 model.summary()
-# model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot), callbacks=[checkpoint])
+model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test, y_test_hot), callbacks=[checkpoint])
 model.compile(loss=keras.losses.categorical_crossentropy,
             optimizer=keras.optimizers.Adam(lr=1e-6),
             metrics=['accuracy'])
@@ -90,6 +90,6 @@ seed = 7
 np.random.seed(seed)
 kf = KFold(n_splits=10,shuffle=False, random_state=seed)
 for train_index , test_index in kf.split(X_test):
-    model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=epochs, verbose=verbose, validation_data=(X_test[test_index], y_test_hot[test_index]), callbacks=[checkpoint])
+    model.fit(X_train, y_train_hot, batch_size=batch_size, epochs=10, verbose=verbose, validation_data=(X_test[test_index], y_test_hot[test_index]), callbacks=[checkpoint])
 model.save('Dog_0512_2.hdf5')
 
