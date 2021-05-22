0baa8630c0d78b8ace9abc12a3e57c9083411ffe
ArthurChien <arthur12394@gmail.com>
2021-05-22 21:27:23 +0800

    Update ans_create_2step.py
    fix some bug

--- ans_create_2step.py
+++ ans_create_2step.py
@@ -9,10 +9,11 @@ import librosa
 import python_speech_features
 import tensorflow.keras as keras
 import csv
+import numpy as np
 test_path = 'public_test/'
 test_list = os.listdir(test_path)
-weight_path = 'weight_acc_97.hdf5'
-weight_path_s2 = 'Dog_0512_2.hdf5'
+weight_path = 'step1.hdf5'
+weight_path_s2 = 'step2.hdf5'
 model = keras.models.load_model(weight_path, compile = False)
 model_step2 = keras.models.load_model(weight_path_s2, compile = False)
 category = ['Filename','Barking', 'Howling', 'Crying', 'COSmoke', 'GlassBreaking', 'Other']
@@ -25,23 +26,29 @@ for data in test_list:
     mel = mel.T
     mel = mel.reshape(1, 120, 499, 1)
     ans = model.predict(mel)
-    
-    if ans.argmax == 0:
+    # print(np.argmax(ans))
+    if np.argmax(ans) == 0:
+        ans = ans.tolist()[0]
         ans2 = model_step2.predict(mel)
+        ans2 = ans2.tolist()[0]
     else:
+        ans = ans.tolist()[0]
         ans2 = [ans[0]/3, ans[0]/3, ans[0]/3]
-#     ans = ans.tolist()
-#     data_l = [data.split('.')[0]] + ans[0]
-#     ans_list.append(data_l)
-    
-# # with open('test.csv','w', newline='') as csvfile:
-# #     writer = csv.writer(csvfile)
-# #     writer.writerows(ans_list)
-# with open('sample_submission.csv') as csvfile:
-#     rows = csv.reader(csvfile)
-#     new = list(rows)
+       
 
-# new[1:10001] = ans_list[1:10001]
-# with open('submission.csv','w', newline='') as csvfile:
+    ans = ans2 + ans[1:-1]
+    
+    data_l = [data.split('.')[0]] + ans
+    ans_list.append(data_l)
+    
+# with open('test.csv','w', newline='') as csvfile:
 #     writer = csv.writer(csvfile)
-#     writer.writerows(new)
+#     writer.writerows(ans_list)
+with open('sample_submission.csv') as csvfile:
+    rows = csv.reader(csvfile)
+    new = list(rows)
+
+new[1:10001] = ans_list[1:10001]
+with open('submission.csv','w', newline='') as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(new)
